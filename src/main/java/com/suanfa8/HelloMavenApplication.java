6f4ebbaada75fdff323a43c682ad51f1ae802abf
liweiwei1419 <liweiwei1419@gmail.com>
2024-10-02 02:50:35 +0800

    实现了一个最最简单的 SpringBoot 项目，仅仅包含一个 HelloWorldController 类，用于返回 Hello World 字符串。

---
+++ src/main/java/com/suanfa8/HelloMavenApplication.java
@@ -0,0 +1,13 @@
+package com.suanfa8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMavenApplication.class, args);
+    }
+
+}

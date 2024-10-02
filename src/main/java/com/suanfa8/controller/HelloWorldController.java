6e69a20c72c16538e2d8ee02378a904808f60179
liweiwei1419 <liweiwei1419@gmail.com>
2024-10-03 00:05:32 +0800

    测试 Github Action。

--- src/main/java/com/suanfa8/controller/HelloWorldController.java
+++ src/main/java/com/suanfa8/controller/HelloWorldController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloWorldController {
 
     // 测试 github action
 
